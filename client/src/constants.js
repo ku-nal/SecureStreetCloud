53fca9972eae872957637d07ac23262879259272
Kunal Rajeshkumar Makwana <kn362288@dal.ca>
2024-08-05 06:06:18 +0530

    Changed URl

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,6 +1,6 @@
-const URL = "http://localhost:7000";
+const URL = "http://10.0.139.217:7000";
 const AWS_URL = "https://tqccfllk4e.execute-api.us-east-1.amazonaws.com/v1"
 module.exports = {
     URL,
     AWS_URL : AWS_URL
-}   
+}
